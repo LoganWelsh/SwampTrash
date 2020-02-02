0800aa29fa1db3b16493a3c83df46517f8ab3ffc
ericderezende <ericbrezende@gmail.com>
2020-02-01 19:02:16 -0500

    GitHub Link Kinda Working

--- frontend/TweetForGreat/src/routes.js
+++ frontend/TweetForGreat/src/routes.js
@@ -1,27 +1,7 @@
-/*!
-
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import Dashboard from "views/Dashboard.jsx";
 import UserProfile from "views/UserProfile.jsx";
 import TableList from "views/TableList.jsx";
-import Typography from "views/Typography.jsx";
-import Icons from "views/Icons.jsx";
-import Maps from "views/Maps.jsx";
-import Notifications from "views/Notifications.jsx";
+
 
 const dashboardRoutes = [
   {
@@ -45,8 +25,6 @@ const dashboardRoutes = [
     component: TableList,
     layout: "/admin"
   },
-  
-  
 ];
 
 export default dashboardRoutes;
