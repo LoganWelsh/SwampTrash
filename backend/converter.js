b92e1260eee6dcb28dc9d5d035bbd0911d82d609
ericderezende <ericbrezende@gmail.com>
2020-02-01 21:42:04 -0500

    Not Important

--- backend/converter.js
+++ backend/converter.js
@@ -1,7 +1,7 @@
  var fs = require('fs');
  var axios = require('axios');
 
-axios.defaults.headers.common['Authorization'] = "cd7ac9ddd0b31b377930f2f181db0c80c523779c";
+axios.defaults.headers.common['Authorization'] = "Bearer ya29.Ima8B-G7oRkXINMvuMnLvLT4bV9FclYGa5kr27U5VDqQGYdgaVJ93CNqrCJSyFLIJwGUoMFLgF41yUm1loL4pQVf5BA-DkfzMp_K46HEJOFj8m2aCa9xL19CIw6Bylpi7-jMaJ9ITrk";
 
 // function to encode file data to base64 encoded string
 const base64_encode = (file) =>{
@@ -12,28 +12,14 @@ const base64_encode = (file) =>{
 }
 
 var base = base64_encode('./images/20200201_175136.jpg');
-axios.post("https://automl.googleapis.com/v1beta1/projects/766644774605/locations/us-central1/models/ICN5802549470285529088:predict", 
+
+  axios.post("https://automl.googleapis.com/v1beta1/projects/766644774605/locations/us-central1/models/ICN5802549470285529088:predict", 
 {
     "payload": {
       "image": {
         "imageBytes": base
       }
     }
-  }, (err, data) => {
-      if (err) throw err;
-      else console.log(data);
-  }
-  );
-
-
-
-//   axios.post("https://automl.googleapis.com/v1beta1/projects/766644774605/locations/us-central1/models/ICN5802549470285529088:predict", 
-// {
-//     "payload": {
-//       "image": {
-//         "imageBytes": base
-//       }
-//     }
-//   }).then(function(res){
-//       console.log(res.data);
-//   });
+  }).then(function(res){
+      console.log(res.data);
+  });
